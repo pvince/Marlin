b0a8fa4b25e09ada2f1f3a1d3c9c48a67c51d190
pvince@gmail.com <vincentp@seapine.com>
2024-08-25 12:13:03 -0400

    Fixed compile error due to uncommenting the wrong line.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1307,7 +1307,7 @@
 //#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
-//#define USE_PROBE_FOR_Z_HOMING
+#define USE_PROBE_FOR_Z_HOMING
 
 /**
  * Z_MIN_PROBE_PIN
@@ -1322,7 +1322,7 @@
  *    - Normally-closed (NC) also connect to GND.
  *    - Normally-open (NO) also connect to 5V.
  */
-#define Z_MIN_PROBE_PIN -1
+// #define Z_MIN_PROBE_PIN -1
 
 /**
  * Probe Type
